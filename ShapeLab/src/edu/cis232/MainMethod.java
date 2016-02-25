17062e383d82050ef391c1f3e6b4eacd914a15fd
SK207_12 <SK207_12@K207_W812.carrollcc.edu>
2016-02-24 21:20:10 -0500

    Final fail

--- ShapeLab/src/edu/cis232/MainMethod.java
+++ ShapeLab/src/edu/cis232/MainMethod.java
@@ -2,13 +2,13 @@ package edu.cis232;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class MainMethod {
 
 	public static void main(String[] args) throws IOException {
-		ArrayList<String> data = new ArrayList<String>();
+		
 		
 		String message;
 		
@@ -16,14 +16,28 @@ public class MainMethod {
 		try{
 			File File = new File("ShapeName.txt");
 			Scanner inputFile = new Scanner(File);
-			
+			int count = 0, i = 0;
+			Shape[] Shapes = new Shape[4];
+			 
 			while (inputFile.hasNext()){
 				message = inputFile.nextLine();
+				StringTokenizer tokens = new StringTokenizer(message,(","));
+				if(message.contains("Circle")){
+					String num = tokens.nextToken();
+					char character = num.toc;
+					if (character.isDigit()){
+						
+					}
+				}
 				
 				System.out.println(message);
+						
+				}
 				
-			}
+			
+			
 			inputFile.close();
+			
 		}
 		
 		catch(IOException IOError){
