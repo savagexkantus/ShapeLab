f035130c4b67da51d7165eb0a81be21e3877b2a8
SK207_12 <SK207_12@K207_W812.carrollcc.edu>
2016-02-24 20:04:26 -0500

    Updated

--- ShapeLab/src/edu/cis232/MainMethod.java
+++ ShapeLab/src/edu/cis232/MainMethod.java
@@ -1,10 +1,29 @@
 package edu.cis232;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainMethod {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		ArrayList<String> data = new ArrayList<String>();
 		
+		String message;
+		
+		
+		try{
+			File inputFile = new File();
+			
+			while (inputFile.hasNext()){
+				
+			}
+		}
+		
+		catch(IOExcetion IOError){
+			
+			message = "Error:" + IOError;
+		}
 		
 		
 	}
