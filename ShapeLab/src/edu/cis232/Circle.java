97b536f3e3889e31c08b9c4b501724a3b6fdb71c
SK207_12 <SK207_12@K207_W812.carrollcc.edu>
2016-02-24 19:18:47 -0500

    This is the lab

---
+++ ShapeLab/src/edu/cis232/Circle.java
@@ -0,0 +1,17 @@
+package edu.cis232;
+
+public class Circle implements Shape {
+	
+	private double area, perimeter;
+	
+	public double getArea(){
+		return area;
+	}
+	
+	public double getPerimeter(){
+		return perimeter;
+	}
+	
+
+
+}
---
+++ ShapeLab/src/edu/cis232/Shape.java
@@ -0,0 +1,9 @@
+package edu.cis232;
+
+public interface Shape {
+	
+	public double getArea();
+		
+	public double getPerimeter();
+
+}
